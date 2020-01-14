2cec7b2f6ae3575cc39714d1236f0554a00f880e
Simon Bengtsson <simonbengtsson@users.noreply.github.com>
2020-01-14 23:37:54 +0100

    Use post request instead of get

--- sender/sw.js
+++ sender/sw.js
@@ -1,1 +1,18 @@
-self.addEventListener('fetch', function(event) {})
+self.importScripts('https://unpkg.com/localforage@1.7.3/dist/localforage.js')
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'POST') {
+    event.respondWith(fetch(event.request))
+    return
+  }
+
+  event.respondWith((async () => {
+    const formData = await event.request.formData()
+    const message = formData.get('text') || 'none'
+
+    await localforage.setItem('message', message)
+    console.log(`Message "${message}" stored`)
+
+    return Response.redirect('/')
+  })())
+})
